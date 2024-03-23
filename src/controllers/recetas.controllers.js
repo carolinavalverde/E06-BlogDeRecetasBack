d62f5474163401af07efab5e54b242b12791f15f
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-22 21:49:54 -0300

    .prueba

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -7,7 +7,7 @@ export const listarRecetas = async (req, res) => {
     res.status(200).json(recetas);
   } catch (err) {
     console.error(err);
-    res.status(400).json({ mensaje: 'Error al buscar las recetas' });
+    res.status(400).json({ mensaje: "Error al buscar las recetas" });
   }
 };
 
@@ -15,12 +15,14 @@ export const obtenerReceta = async (req, res) => {
   try {
     const recetaBuscada = await Receta.findById(req.params.id);
     if (!recetaBuscada) {
-      return res.status(404).json({ mensaje: 'El id enviado no corresponde a ninguna receta' });
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ninguna receta" });
     }
     res.status(200).json(recetaBuscada);
   } catch (err) {
     console.error(err);
-    res.status(400).json({ mensaje: 'Error al obtener la receta' });
+    res.status(400).json({ mensaje: "Error al obtener la receta" });
   }
 };
 
@@ -28,10 +30,10 @@ export const crearReceta = async (req, res) => {
   try {
     const nuevaReceta = new Receta(req.body);
     await nuevaReceta.save();
-    res.status(201).json({ mensaje: 'La receta fue creada correctamente' });
+    res.status(201).json({ mensaje: "La receta fue creada correctamente" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ mensaje: 'Error al crear la receta' });
+    res.status(500).json({ mensaje: "Error al crear la receta" });
   }
 };
 
@@ -39,13 +41,15 @@ export const editarReceta = async (req, res) => {
   try {
     const recetaBuscada = await Receta.findById(req.params.id);
     if (!recetaBuscada) {
-      return res.status(404).json({ mensaje: 'El id enviado no corresponde a ninguna receta' });
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ninguna receta" });
     }
     await Receta.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).json({ mensaje: 'La receta fue editada correctamente' });
+    res.status(200).json({ mensaje: "La receta fue editada correctamente" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ mensaje: 'Error al editar la receta' });
+    res.status(500).json({ mensaje: "Error al editar la receta" });
   }
 };
 
@@ -53,12 +57,14 @@ export const borrarReceta = async (req, res) => {
   try {
     const recetaBuscada = await Receta.findById(req.params.id);
     if (!recetaBuscada) {
-      return res.status(404).json({ mensaje: 'El id enviado no corresponde a ninguna receta' });
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ninguna receta" });
     }
     await Receta.findByIdAndDelete(req.params.id);
-    res.status(200).json({ mensaje: 'La receta fue eliminada correctamente' });
+    res.status(200).json({ mensaje: "La receta fue eliminada correctamente" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ mensaje: 'Error al eliminar la receta' });
+    res.status(500).json({ mensaje: "Error al eliminar la receta" });
   }
 };
