4325c987c471e10ec75e7bfd72786770787724c3
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-22 21:33:46 -0300

    agrego schema de receta

---
+++ src/database/models/receta.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const recetaSchema = new Schema({
+  nombreReceta: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+    unique: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (data) => {
+        const pattern = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)$/;
+        return pattern.test(data);
+      },
+      message:
+        "Debe ingresar una URL válida con una imagen en formato (jpg | jpeg | gif | png)",
+    },
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 200,
+  },
+  receta: {
+    type: String,
+    required: true,
+    minLength: 30,
+    maxLength: 200,
+  },
+});
+
+const Receta = mongoose.model("Receta", recetaSchema);
+
+export default Receta;
